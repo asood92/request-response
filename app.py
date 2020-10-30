f14d18764d0455ba076de67766db9ded2a13e2a1
Amman Sood <392075+asood92@users.noreply.github.com>
2020-10-29 20:36:50 -0400

    animal and dessert done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def homepage():
     """Shows a greeting to the user."""
     return "Are you there, world? It's me, Ducky!"
+
+
+@app.route("/penguins")
+def myFavoriteAnimal():
+    return "Penguins are cute!"
+
+
+@app.route("/dessert/<users_dessert>")
+def userDessert(users_dessert):
+    """Display a message tot he user that changes based on their favorite dessert"""
+    return f"How did you know I liked {users_dessert}?"
 
 
 if __name__ == "__main__":
